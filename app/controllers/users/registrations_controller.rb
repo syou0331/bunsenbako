83889c2d84032fc31d7706dcdd52cb7e4c6ca122
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-02-03 18:18:20 +0900

    アカウント編集後、プロフィール画面に移動する様変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  protected
+
+  def after_update_path_for(resource)
+    user_path(id: current_user.id)
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root          to: 'top#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
