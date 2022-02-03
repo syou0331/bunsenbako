ab1c44809297ee8b5cf92a9885f3abd194f8937b
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-02-03 16:07:00 +0900

    users tableのカラムを編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_022858) do
+ActiveRecord::Schema.define(version: 2022_02_03_050022) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,7 +41,8 @@ ActiveRecord::Schema.define(version: 2022_02_02_022858) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.string "name", null: false
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
