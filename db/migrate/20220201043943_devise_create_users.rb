46b0afdce178b814f7127329ea6ce3b0bdf63a54
syou0331 <86101556+syou0331@users.noreply.github.com>
2022-02-03 20:31:10 +0900

    重複しているカラムを削除

--- db/migrate/20220201043943_devise_create_users.rb
+++ db/migrate/20220201043943_devise_create_users.rb
@@ -3,8 +3,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.text :profile
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
